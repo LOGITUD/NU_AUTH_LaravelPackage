34eae8b1e4e73742e7652c7c02d1889345352a3b
zorx <oudjehih@zedx.org>
2016-06-01 13:14:18 +0200

    add auth middleware

---
+++ src/Middleware/Authenticate.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Numesia\NUAuth\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Numesia\NUAuth\NUAuth;
+
+class Authenticate
+{
+    /**
+     * Create a new instance.
+     *
+     * @param \Numesia\NUAuth\NUAuth  $auth
+     */
+    public function __construct(NUAuth $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        try {
+            $user = $this->auth->user();
+        } catch (TokenExpiredException $e) {
+            return $this->respond('tymon.jwt.expired', 'token_expired', $e->getStatusCode(), [$e]);
+        } catch (JWTException $e) {
+            return $this->respond('tymon.jwt.invalid', 'token_invalid', $e->getStatusCode(), [$e]);
+        }
+
+        event('tymon.jwt.valid', $user);
+
+        return $next($request);
+    }
+
+    /**
+     * Fire event and return the response.
+     *
+     * @param  string   $event
+     * @param  string   $error
+     * @param  int  $status
+     * @param  array    $payload
+     * @return mixed
+     */
+    protected function respond($event, $error, $status, $payload = [])
+    {
+        event($event, $payload);
+        return response()->json(['error' => $error], $status);
+    }
+}
